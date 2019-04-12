dec881edb18dbbc3e866ca8b611a72795f5a3a9f
Luiz Marcelo G Neves <luiz.neves@sage.com>
2019-04-12 15:36:57 -0300

    Ajustes Gerais

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -1,20 +1,18 @@
-const Box = require('../models/Box');
+const Box = require("../models/Box");
 
-class BoxController{
-    async store (req, res){
-        const box = await Box.create(req.body); 
-            return res.json(box);
-    };
+class BoxController {
+  async store(req, res) {
+    const box = await Box.create(req.body);
+    return res.json(box);
+  }
+  async show(req, res) {
+    const box = await Box.findById(req.params.id).populate({
+      path: "files",
+      options: { sort: { createdAt: -1 } }
+    });
 
-    async show (req, res){
-        const box = await Box.findById(req.params.id).populate({
-            path: 'files',
-            options: {sort: {createdAt: -1 } }
-        });
-         
-        return res.json(box);
-    };
-
-};
+    return res.json(box);
+  }
+}
 
 module.exports = new BoxController();
--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -1,21 +1,22 @@
-const File = require('../models/File');
-const Box = require('../models/Box');
+const Box = require("../models/Box");
+const File = require("../models/File");
 
-class FileController{
-    async store (req, res){
-        const box = await Box.findById(req.params.id);
-        const file = await File.create({
-            title: req.file.originalname,
-            path: req.file.key
-        });
+class FileController {
+  async store(req, res) {
+    const box = await Box.findById(req.params.id);
+    const file = await File.create({
+      title: req.file.originalname,
+      path: req.file.key
+    });
 
-        box.files.push(file);
-        await box.save();
+    box.files.push(file);
 
-        req.io.sockets.in(box._id).emit('file', file);
+    await box.save();
 
-        return res.json(file);
-    };
-};
+    req.io.sockets.in(box._id).emit("file", file);
+
+    return res.json(file);
+  }
+}
 
 module.exports = new FileController();
